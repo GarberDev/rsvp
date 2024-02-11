76c94a393fa74d95f73b2d917cc6c1e74b67ac95
Justin Garber <JustinGarber@outlook.com>
2024-02-10 22:28:25 -0800

    more

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Typography, Container, Paper, Box } from "@mui/material";
+import { Button, Typography, Paper, Box } from "@mui/material";
+import Link from "@mui/material/Link";
 
 const LandingPage = () => {
   const navigate = useNavigate();
@@ -9,42 +10,91 @@ const LandingPage = () => {
     navigate("/rsvp");
   };
 
+  const address = "24316 W Venturi Dr, Valencia, CA 91355";
+  const eventTitle = encodeURIComponent("Aurora's Fourth Birthday");
+  const eventDetails = encodeURIComponent(
+    "Come celebrate Aurora's fourth birthday with us! No gifts are necessary, your presence is the most cherished gift. For more details visit: https://www.jaimiegarber.com"
+  );
+
+  const location = encodeURIComponent(address);
+  const startTime = "20240316T143000";
+  const endTime = "20240316T170000";
+  const calendarEventUrl = `https://www.google.com/calendar/render?action=TEMPLATE&text=${eventTitle}&details=${eventDetails}&location=${location}&dates=${startTime}/${endTime}`;
+  const mapsUrl = `https://www.google.com/maps/search/?api=1&query=${location}`;
+
   return (
-    <Paper elevation={3} sx={{ p: 4, mt: 5 }}>
-      <Typography variant="h4" gutterBottom>
-        You're Cordially Invited! Aurora's Fourth Birthday
-      </Typography>
-      <Typography paragraph>
-        Celebrate with us as we mark a special occasion for Aurora. Your
-        presence is the most cherished gift, so no other gifts are necessary.
-      </Typography>
-      <Box mb={2}>
-        <Typography variant="body1">
-          <strong>Date:</strong> March 16th, 2024
+    <div
+      style={{
+        backgroundImage: `url('/wallpaper_paw.png')`, // Accessing image from the public folder
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh", // This sets the height to full viewport height
+        width: "100%",
+        position: "absolute",
+      }}
+    >
+      <Paper
+        elevation={3}
+        sx={{ p: 4, mt: 5, background: "rgba(255, 255, 255, 0.8)" }}
+      >
+        {" "}
+        <Typography variant="h4" gutterBottom>
+          You're Cordially Invited! Aurora's Fourth Birthday
         </Typography>
-        <Typography variant="body1">
-          <strong>Time:</strong> 2:30pm
+        <Typography paragraph>
+          Celebrate with us as we mark a special occasion for Aurora. Your
+          presence is the most cherished gift, so no other gifts are necessary.
         </Typography>
-        <Typography variant="body1">
-          <strong>Event Address:</strong> 24316 W Venturi Dr, Valencia, CA 91355
+        <Box mb={2}>
+          <Typography variant="body1">
+            <strong>Date:</strong> March 16th, 2024
+          </Typography>
+          <Typography variant="body1">
+            <strong>Time:</strong> 2:30pm
+          </Typography>
+          <Typography variant="body1">
+            <strong>Event Address:</strong> {address}
+          </Typography>
+          <Typography variant="body1">
+            <strong>Contact:</strong> (661) 714-3945
+          </Typography>
+        </Box>
+        <Button variant="contained" color="primary" onClick={handleRSVPClick}>
+          Click Here to RSVP
+        </Button>
+        {/* Add to Google Calendar Button */}
+        <Box mt={2} display="flex" gap={2}>
+          <Button
+            variant="outlined"
+            color="secondary"
+            href={calendarEventUrl}
+            target="_blank"
+            rel="noopener"
+          >
+            Google Calendar
+          </Button>
+          {/* Open in Maps Button */}
+          <Button
+            variant="outlined"
+            color="secondary"
+            href={mapsUrl}
+            target="_blank"
+            rel="noopener"
+          >
+            Open in Maps
+          </Button>
+        </Box>
+        <Typography paragraph mt={2}>
+          In lieu of gifts, we welcome you to bring your child's favorite book
+          as a gift. Aurora's passion for reading will be ignited by the stories
+          in the books you give her.
         </Typography>
-        <Typography variant="body1">
-          <strong>Contact:</strong> (661) 714-3945
+        <Typography paragraph>
+          We eagerly await your company to fill the day with laughter, stories,
+          and the joy of togetherness.
         </Typography>
-      </Box>
-      <Button variant="contained" color="primary" onClick={handleRSVPClick}>
-        Click Here to RSVP
-      </Button>
-      <Typography paragraph mt={2}>
-        In lieu of gifts, we welcome you to bring your child's favorite book as
-        a gift. Aurora's passion for reading will be ignited by the stories in
-        the books you give her.
-      </Typography>
-      <Typography paragraph>
-        We eagerly await your company to fill the day with laughter, stories,
-        and the joy of togetherness.
-      </Typography>
-    </Paper>
+      </Paper>
+    </div>
   );
 };
 
--- src/WizardPages/RSVPWizard.js
+++ src/WizardPages/RSVPWizard.js
@@ -1,137 +1,239 @@
 import React, { useState } from "react";
 import {
-  Container,
-  Step,
-  StepLabel,
-  Stepper,
   Button,
-  Typography,
   TextField,
+  Typography,
+  Container,
+  FormControlLabel,
+  Switch,
   Box,
+  Paper,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-
-const steps = [
-  "Number of Guests",
-  "Names and Ages",
-  "Food Restrictions or Comments",
-  "Email Confirmation",
-  "Thank You",
-];
+import emailjs from "emailjs-com";
+import backgroundImg from "./just_paws.png";
 
 const RSVPWizard = () => {
-  const [activeStep, setActiveStep] = useState(0);
-  const [formData, setFormData] = useState({
-    numberOfGuests: "",
-    names: "",
-    comments: "",
-    email: "",
-  });
+  const [step, setStep] = useState(1);
+  const [numberOfPeople, setNumberOfPeople] = useState("");
+  const [names, setNames] = useState([{ name: "", isAdult: true }]);
+  const [email, setEmail] = useState("");
   const navigate = useNavigate();
 
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  const nextStep = () => setStep(step + 1);
+  const prevStep = () => setStep(step - 1);
+  const handleChangeNumberOfPeople = (e) => {
+    setNumberOfPeople(e.target.value);
+    const newNames = Array.from(
+      { length: parseInt(e.target.value, 10) },
+      () => ({ name: "", isAdult: true })
+    );
+    setNames(newNames);
   };
 
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  const handleNameChange = (index, event) => {
+    const updatedNames = names.map((item, i) =>
+      i === index ? { ...item, name: event.target.value } : item
+    );
+    setNames(updatedNames);
   };
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+  const handleToggleChange = (index) => {
+    const updatedNames = names.map((item, i) =>
+      i === index ? { ...item, isAdult: !item.isAdult } : item
+    );
+    setNames(updatedNames);
+  };
+  const handleSubmit = () => {
+    const formData = {
+      numberOfPeople,
+      names,
+      email,
+    };
+
+    // Sending data to Google Sheets
+    fetch(
+      "https://script.google.com/macros/s/AKfycbzzf9IXQUL0-KBxgXFk2YagMecKSUikSbNWethqLt-TOlCr3GC9e-T4Cq_rS7oGhada/exec",
+      {
+        method: "POST",
+        mode: "no-cors", // Required for CORS policies in Google Apps Script
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+        contentType: "application/json",
+        body: JSON.stringify(formData),
+      }
+    )
+      .then((response) => {
+        console.log("Success:", response);
+
+        if (email.trim() !== "") {
+          const templateParams = {
+            email,
+            numberOfPeople,
+            names: names
+              .map(
+                (name) => `${name.isAdult ? "Adult" : "Child"}: ${name.name}`
+              )
+              .join(", "),
+          };
+
+          const serviceId = "service_uq8fedr";
+          const templateId = "template_vvwgrrm";
+          const userId = "fTLbPrJxkPeJbTBYv";
+
+          // Send email using EmailJS
+          emailjs
+            .send(serviceId, templateId, templateParams, userId)
+            .then((emailResponse) => {
+              console.log("Email successfully sent!", emailResponse);
+            })
+            .catch((emailError) =>
+              console.error("Failed to send email. Error:", emailError)
+            );
+        }
+
+        // Redirect to home page after submitting the form and optionally sending the email
+        navigate("/");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
 
-  const getStepContent = (stepIndex) => {
-    switch (stepIndex) {
-      case 0:
-        return (
-          <TextField
-            label="Number of Guests"
-            name="numberOfGuests"
-            value={formData.numberOfGuests}
-            onChange={handleChange}
-            fullWidth
-          />
-        );
-      case 1:
-        return (
-          <TextField
-            label="Names and Ages (e.g., John-34, Jane-30, Alex-5)"
-            name="names"
-            value={formData.names}
-            onChange={handleChange}
-            fullWidth
-            multiline
-          />
-        );
-      case 2:
-        return (
-          <TextField
-            label="Food Restrictions or Comments"
-            name="comments"
-            value={formData.comments}
-            onChange={handleChange}
-            fullWidth
-            multiline
-          />
-        );
-      case 3:
-        return (
-          <TextField
-            label="Your Email for Confirmation"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            fullWidth
-          />
-        );
-      case 4:
-        return (
-          <Typography variant="body1">
-            Thank you for your RSVP!{" "}
-            {formData.email &&
-              `A confirmation email will be sent to ${formData.email}.`}
-          </Typography>
-        );
-      default:
-        return "Unknown step";
-    }
-  };
+  switch (step) {
+    case 1:
+      return (
+        <div
+          style={{
+            backgroundImage: `url(${backgroundImg})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            minHeight: "100vh", // Ensure it covers the full viewport height
+          }}
+        >
+          <Paper
+            elevation={3}
+            sx={{ p: 4, mt: 5, background: "rgba(255, 255, 255, 0.8)" }}
+          >
+            {" "}
+            <Container maxWidth="sm">
+              <Typography variant="h6" align="center">
+                How many people are coming?
+              </Typography>
+              <TextField
+                label="Number of People"
+                type="number"
+                fullWidth
+                margin="normal"
+                value={numberOfPeople}
+                onChange={handleChangeNumberOfPeople}
+              />
+              <Box mt={2} display="flex" justifyContent="space-between" gap={2}>
+                <Button variant="contained" onClick={() => navigate("/")}>
+                  Home
+                </Button>
+                <Button variant="contained" onClick={nextStep}>
+                  Next
+                </Button>
+              </Box>
+            </Container>
+          </Paper>
+        </div>
+      );
 
-  return (
-    <Container maxWidth="sm">
-      <Stepper activeStep={activeStep} alternativeLabel>
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-      <Box sx={{ mt: 3 }}>
-        {getStepContent(activeStep)}
-        <Box sx={{ display: "flex", justifyContent: "flex-end", mt: 2 }}>
-          {activeStep > 0 && (
-            <Button onClick={handleBack} sx={{ mr: 1 }}>
-              Back
-            </Button>
-          )}
-          {activeStep < steps.length - 1 ? (
-            <Button variant="contained" onClick={handleNext}>
-              Next
-            </Button>
-          ) : (
-            <Button variant="contained" onClick={() => navigate("/")}>
-              Return to Home
-            </Button>
-          )}
-        </Box>
-      </Box>
-    </Container>
-  );
+    case 2:
+      return (
+        <div
+          style={{
+            backgroundImage: `url(${backgroundImg})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            minHeight: "100vh", // Ensure it covers the full viewport height
+          }}
+        >
+          <Paper
+            elevation={3}
+            sx={{ p: 4, mt: 5, background: "rgba(255, 255, 255, 0.8)" }}
+          >
+            <Container maxWidth="sm">
+              <Typography align="center" variant="h6">
+                Enter Names & Adult or Child
+              </Typography>
+              {names.map((item, index) => (
+                <div key={index}>
+                  <hr />
+
+                  <TextField
+                    label={`Guest Name ${index + 1}`}
+                    fullWidth
+                    margin="normal"
+                    value={item.name}
+                    onChange={(e) => handleNameChange(index, e)}
+                  />
+                  <FormControlLabel
+                    control={
+                      <Switch
+                        checked={item.isAdult}
+                        onChange={() => handleToggleChange(index)}
+                      />
+                    }
+                    label={item.isAdult ? "Adult" : "Child"}
+                  />
+                </div>
+              ))}
+
+              <Box mt={2} display="flex" justifyContent="space-between" gap={2}>
+                <Button variant="contained" onClick={prevStep}>
+                  Back
+                </Button>
+                <Button variant="contained" onClick={nextStep}>
+                  Next
+                </Button>
+              </Box>
+            </Container>
+          </Paper>
+        </div>
+      );
+    case 3:
+      return (
+        <div
+          style={{
+            backgroundImage: `url(${backgroundImg})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            minHeight: "100vh", // Ensure it covers the full viewport height
+          }}
+        >
+          <Paper
+            elevation={3}
+            sx={{ p: 4, mt: 5, background: "rgba(255, 255, 255, 0.8)" }}
+          >
+            <Container maxWidth="sm">
+              <Typography align="center" variant="h6">
+                Email Confirmation *not required*
+              </Typography>
+              <TextField
+                label="Email"
+                fullWidth
+                margin="normal"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Box mt={2} display="flex" justifyContent="space-between" gap={2}>
+                <Button variant="contained" onClick={prevStep}>
+                  Back
+                </Button>
+                <Button variant="contained" onClick={handleSubmit}>
+                  Confirm
+                </Button>
+              </Box>
+            </Container>
+          </Paper>
+        </div>
+      );
+    default:
+      return <div>Unknown step</div>;
+  }
 };
 
 export default RSVPWizard;
