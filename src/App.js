65ef27d765481ab9c1215dbc6b4c6ff177eb1d80
Justin Garber <JustinGarber@outlook.com>
2024-02-15 18:55:24 -0800

    analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import LandingPage from "./LandingPage";
 import RSVPWizard from "./WizardPages/RSVPWizard";
+import ReactGA from "react-ga4"; // Use the default export
+
+// Your Google Analytics 4 measurement ID
+const measurementId = "G-YKBSL15WCH";
+
+// Initialize Google Analytics
+ReactGA.initialize(measurementId);
+console.log("Google Analytics initialized with ID:", measurementId);
+
+function useTrackPageViews() {
+  let location = useLocation();
+
+  useEffect(() => {
+    const pagePath = location.pathname + location.search;
+    // Track page view with react-ga4 directly
+    ReactGA.send({
+      hitType: "pageview",
+      page: pagePath,
+    });
+    console.log(`Page view tracked: ${pagePath}`);
+  }, [location]);
+}
+
+function TrackPageViews() {
+  useTrackPageViews(); // Use the hook to track page views
+  return null; // This component doesn't render anything
+}
 
 function App() {
   return (
     <Router>
+      <TrackPageViews /> {/* Component to handle page view tracking */}
       <Routes>
         <Route path="/" element={<LandingPage />} exact />
         <Route path="/rsvp" element={<RSVPWizard />} />
